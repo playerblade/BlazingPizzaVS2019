3437616964ced642964a59eca78343108df1a2bc
playerblade <evotar156@gmail.com>
2020-12-04 10:44:08 -0400

    First Commit

---
+++ BlazingPizzaVS2019/Contexts/PizzaContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazingPizzaVS2019.Models;
+
+namespace BlazingPizzaVS2019.Contexts
+{
+    public class PizzaContext: DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> data) : base(data)
+        {
+
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+
+    }
+}
